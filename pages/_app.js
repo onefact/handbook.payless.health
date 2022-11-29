85c1df25d29754c2415bb1144a97f8ef997ba0dc
Jaan Altosaar <altosaar@users.noreply.github.com>
2022-11-29 19:23:54 +0700

    add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -122,7 +122,7 @@ export default function MyApp(props) {
         <meta name="title" content={title} />
         <meta name="description" content={description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://markdoc.dev" />
+        <meta property="og:url" content="https://payless.health" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta
