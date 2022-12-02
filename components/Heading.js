b5e5a24eeb11b1b16f5d7d63fbd7fa6821d6b6b6
Jaan Altosaar <jaan@onefact.org>
2022-12-02 13:37:25 +0700

    fix unused imporlt

--- components/Heading.js
+++ components/Heading.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 export function Heading({ id = '', level = 1, children, className }) {
-  const router = useRouter();
+  // const router = useRouter();
   const Component = `h${level}`;
 
   const isDocs = true 
